e4632477109183d45027a7a9f4e5de49f927481c
OZ <normandiggs@gmail.com>
2020-07-21 21:35:26 +0200

    make it little bit more reactive

---
+++ interactive.jsx
@@ -0,0 +1,57 @@
+'use strict';
+
+const e = React.createElement;
+
+class StoryButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  render() {
+    return (
+        <section>
+          <button className={'btn '+(this.state.open && 'btn_pressed')} type="button" onClick={()=>this.setState({open: !this.state.open})}>Story</button>
+          { this.state.open &&
+            (<div className="pt1">
+              My passion to programming has started in school.<br/>
+              In 2004 I've started making money by making websites.<br/>
+              One of my first tools was FTP-editor - tool to upload, remove and edit files (mostly PHP-scripts). It was designed to be used from a mobile phone (in 2004 not every russian youngster had a computer, so "mobile first" for us was more than
+              motto).<br/>
+              But the most interesting thing about that editor - development of this tool had just 3 computer hours and rest of the code was written using this editor itself (yes, from a mobile phone).<br/>
+              This tool helped dozens of people to create their first web sites (or WAP-sites).<br/>
+              It was challenging but extremely rewarding.
+            </div>)
+          }
+        </section>
+    );
+  }
+}
+
+class RevealEmail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+    this.email = props && props.email && props.email.replace(' at ', '@').replace('_dotcom','.com').replace('_dotdev','.dev'); 
+  }
+
+  render() {
+    return (
+        <span>
+          {
+            this.state.open 
+                ? <strong>{this.email}</strong>
+                : <button className="btn sm" type="button" onClick={()=>this.setState({open: true})}>Reveal email</button>
+          }
+        </span>
+    );
+  }
+}
+
+ReactDOM.render(e(StoryButton), document.getElementById('story_btn'));
+ReactDOM.render(
+    <RevealEmail email="bthole at purchaseclinic_dotcom"></RevealEmail>, 
+    document.getElementById('reveal_email_1'));
+ReactDOM.render(
+    <RevealEmail email="oz at jamm_dotdev"></RevealEmail>, 
+    document.getElementById('reveal_email_2'));
