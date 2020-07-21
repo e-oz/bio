83d50357e0c821f8341276b34f8426d8a14e472f
OZ <normandiggs@gmail.com>
2020-07-21 21:41:41 +0200

    corrections by grammarly

--- interactive.jsx
+++ interactive.jsx
@@ -14,11 +14,10 @@ class StoryButton extends React.Component {
           <button className={'btn '+(this.state.open && 'btn_pressed')} type="button" onClick={()=>this.setState({open: !this.state.open})}>Story</button>
           { this.state.open &&
             (<div className="pt1">
-              My passion to programming has started in school.<br/>
+              My passion for programming has started in school.<br/>
               In 2004 I've started making money by making websites.<br/>
-              One of my first tools was FTP-editor - tool to upload, remove and edit files (mostly PHP-scripts). It was designed to be used from a mobile phone (in 2004 not every russian youngster had a computer, so "mobile first" for us was more than
-              motto).<br/>
-              But the most interesting thing about that editor - development of this tool had just 3 computer hours and rest of the code was written using this editor itself (yes, from a mobile phone).<br/>
+              One of my first tools was FTP-editor - tool to upload, remove and edit files (mostly PHP-scripts). It was designed to be used from a mobile phone (in 2004, not every Russian youngster had a computer, so "mobile-first" for us was more than a motto).<br/>
+              But the most interesting thing about that editor - development of this tool had just 3 computer hours and the rest of the code was written using this editor itself (yes, from a mobile phone).<br/>
               This tool helped dozens of people to create their first web sites (or WAP-sites).<br/>
               It was challenging but extremely rewarding.
             </div>)
